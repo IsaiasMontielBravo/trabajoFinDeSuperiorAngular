2e81670d9cb4bc9af9102a1a7550924fd92baa29
unknown <isimetallica@hotmail.com>
2018-06-19 21:36:48 +0200

    hotfixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,7 +9,7 @@ from "./components/index.paginas";
 const app_routes: Routes = [
   { path: 'home', component: PortafolioComponent },
   { path: 'about', component: AboutComponent },
-  { path: '**', pathMatch: 'full', redirectTo: '' }
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
 export const app_routing = RouterModule.forRoot(app_routes, {useHash:true});
